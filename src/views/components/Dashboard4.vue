b863d079cad71c167b47274cb75728b452c66ca9
xj-hash <4143628@qq>
2021-03-30 09:52:30 +0800

    仓库handInDetails的表单加入seqpartment

--- src/views/components/Dashboard4.vue
+++ src/views/components/Dashboard4.vue
@@ -7,14 +7,16 @@
       <div class="arrow-top-right"></div>
       <div class="textArray">
         <div contenteditable="true" style="width:100%; height:100%;color: #ffffff;" class="w-e-text" id="text-elem9085944164005537">
-          <p>&nbsp; &nbsp; 通知</p>
-          <p>&nbsp; &nbsp; 螺丝机-OEE 人工点选项实时上传逻辑现已更新至E3-1# 机，请现场生产配合测试。<br></p>
-          <p>&nbsp; &nbsp; 新点选逻辑为：</p>
-          <p><font color="#c24f4a">&nbsp; &nbsp;吃饭休息时收线停机，停机后立即点选停机原因（吃饭休息），随后正常吃饭休息，回线时正常开机即可。</font><br></p>
-          <p><br/></p>
-          <p><br/></p>
-          <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; SME<font color="#c24f4a"><br></font></p>
-          <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 2020-12-30<br></p></div>
+          <p>&nbsp; &nbsp; 通知:</p>
+          <p>&nbsp; &nbsp; 2021【捷普杯】员工技能大赛 2月28日报名开启，截止日期至3月5日<br>
+          &nbsp; &nbsp; 赛程安排：<br>
+          &nbsp; &nbsp; 报名(2021/2/25-3/5) ->  开幕式(2021/3/23) ->  初赛(2021/3/29-2021/4/1) -><br>
+          &nbsp; &nbsp; 决赛(2021/4/19-2021/4/24) ->  闭幕式(2021/4/28)</p>
+
+          <p><font color="aqua">&nbsp; &nbsp; PBI技能培训（开课时间3月10日下午13:00-16:00；报名截止时间3月4日8:00；报名方式：发送姓名+工号+邮箱账号至Silong_He@jabil.com）<br>
+            &nbsp; &nbsp;</font></p>
+          <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; SME<font color="#c24f4a"><br></font></p>
+          <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 2021-3-02<br></p></div>
       </div>
     </div>
   </div>
